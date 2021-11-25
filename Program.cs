6cb6e9d334ccdf9f25cb60c0a62e21c034686cca
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-11-25 18:02:53 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EstimaEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double euler = 0;
+
+            Console.WriteLine("-- Estimador de Euler --");
+
+            Console.Write("Quantas interaçoes (<=33)? ");
+            double interaçoes = Convert.ToDouble(Console.ReadLine());
+              for (int i = 0; i < interaçoes; i++)
+            {
+                euler += 1/retornaFat(i);
+            }
+
+            Console.WriteLine($"Estimativa: {euler:N10}");
+        }
+
+        static double retornaFat(int valor)
+        {
+            double fat = 1;
+
+            if (valor > 0)
+            {
+                for (int i = 2; i <= valor; i++)
+                {
+                    fat *= i;
+                }
+            }
+
+            return fat;
+        }
+    }
+}
